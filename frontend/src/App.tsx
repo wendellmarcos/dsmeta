0f9b6543dbebb9898435f68b9f4bbb048a7ffe89
Wendell Marcos <wendell.marcos.code@gmail.com>
2022-11-20 17:52:40 +0000

    Subindo componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
-function App() {
-  return(
-    <h1> Construindo! </h1>
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
+
+
+function App() {
+  return  (
+    <>
+      <Header />
+      <main>
+       <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
+
 export default App
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg';
+
+import './styles.css'
+
+function Header() {
+   return(
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src= {logo} alt="DSMeta" />
+                <h1>W.M.W</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/wendellmarcosw.ig">@wendellmarcosw</a>
+                </p>
+            </div>
+        </header>
+    )
+
+}
+
+export default Header

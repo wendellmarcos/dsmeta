2e52177e50f9aba3d92607c6a5049cefe25d34f3
Wendell Marcos <wendell.marcos.code@gmail.com>
2022-11-21 14:59:45 +0000

    h2

---
+++ backend/src/main/java/com/wmw/dsmeta/entities/Sale.java
@@ -0,0 +1,77 @@
+package com.wmw.dsmeta.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sellerName;
+	private Integer visit;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	public Sale() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public Integer getVisit() {
+		return visit;
+	}
+
+	public void setVisit(Integer visit) {
+		this.visit = visit;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	
+	
+}
